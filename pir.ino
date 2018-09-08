cf5c027de242546011bf8c248923163e91cb25d5
Shreyas097 <38488824+Shreyas097@users.noreply.github.com>
2018-09-08 00:53:49 -0300

    Add files via upload

---
+++ pir.ino
@@ -0,0 +1,23 @@
+int led = 13;                // the pin that the LED is atteched to
+int sensor = 7;              // the pin that the sensor is atteched to
+int val = 0;                 // variable to store the sensor status (value)
+
+void setup() {
+  pinMode(led, OUTPUT);      // initalize LED as an output
+  pinMode(sensor, INPUT);    // initialize sensor as an input
+  Serial.begin(9600);        // initialize serial
+}
+
+void loop(){
+  val = digitalRead(sensor);   // read sensor value
+  if (val == HIGH) {           // check if the sensor is HIGH
+    digitalWrite(led, HIGH);   // turn LED ON
+    delay(1000);                // delay 100 milliseconds 
+    Serial.println("Motion detected!"); 
+  } 
+  else {
+      digitalWrite(led, LOW); // turn LED OFF
+      delay(1000);             // delay 200 milliseconds 
+        Serial.println("Motion stopped!");
+  }
+}
