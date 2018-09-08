29ec96235c51c41a561714d913e50a0e22012dcf
Shreyas097 <38488824+Shreyas097@users.noreply.github.com>
2018-09-08 00:58:28 -0300

    Add files via upload

---
+++ SSR_code.ino.ino
@@ -0,0 +1,16 @@
+int relay = 7;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(relay,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(relay,LOW); // Turns ON Relays 1
+  Serial.println("Light On");
+  delay(10000); // Wait 2 seconds
+  digitalWrite(relay,HIGH); // Turns Relay Off
+  Serial.println("Light Off");
+  delay(10000);
+}
