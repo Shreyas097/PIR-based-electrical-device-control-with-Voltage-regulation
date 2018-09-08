ced6b5215a06a757b364e66ae9cb032f4876c738
Shreyas097 <38488824+Shreyas097@users.noreply.github.com>
2018-09-08 00:53:26 -0300

    Add files via upload

---
+++ LM35.ino
@@ -0,0 +1,30 @@
+int val;
+int tempPin = A5;
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(tempPin,INPUT);
+}
+void loop()
+{
+  val = analogRead(tempPin);
+  float mv = ( val/1024.0)*5000; 
+  float cel = mv/10;
+
+  Serial.print("TEMPRATURE = ");
+  Serial.print(cel);
+  Serial.print("*C");
+  Serial.println();
+  delay(1000);
+
+  //uncomment this to get temperature in farenhite 
+  /*Serial.print("TEMPRATURE = ");
+  Serial.print(farh);
+  Serial.print("*F");
+  Serial.println();*/
+  if (cel > 31)
+  {
+     Serial.println("Light On");
+  }
+}
